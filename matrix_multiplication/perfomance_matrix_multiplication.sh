208c394cfa79928672f7ff70a6c8d8945259221c
nickdn <nickdndev@gmail.com>
2021-10-09 16:11:23 +0300

    fix range according task

--- matrix_multiplication/perfomance_matrix_multiplication.sh
+++ matrix_multiplication/perfomance_matrix_multiplication.sh
@@ -4,7 +4,7 @@ for optimization_flag in '-g' '-O3'
 do
   echo "Optimization flag : $optimization_flag"
   make OPTIMIZE=$optimization_flag
-  for matrix_size in 512 1024 2048 4096
+  for matrix_size in 500 512 1000 1024 2000 2048
   do
     ./matrix_multiplication $matrix_size
     ./matrix_multiplication $matrix_size strassen
